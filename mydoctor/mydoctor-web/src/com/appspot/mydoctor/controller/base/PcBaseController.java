f752476a4b936e19fd21b24f53cd240a8898e2ed
kana2ggl <kana2ggl@gmail.com>
2011-03-22 02:29:21 +0000

    move to http://code.google.com/p/my-doctor/

--- mydoctor/mydoctor-web/src/com/appspot/mydoctor/controller/base/PcBaseController.java
+++
@@ -1,9 +0,0 @@
-package com.appspot.mydoctor.controller.base;
-
-public abstract class PcBaseController extends BaseController {
-
-    @Override
-    protected String getJspDir() {
-        return "/jsp/pc/";
-    }
-}
--- mydoctor/mydoctor-web/src/com/appspot/mydoctor/controller/base/SmartPhoneBaseController.java
+++
@@ -1,9 +0,0 @@
-package com.appspot.mydoctor.controller.base;
-
-public abstract class SmartPhoneBaseController extends BaseController {
-
-    @Override
-    protected String getJspDir() {
-        return "/jsp/sp/";
-    }
-}
